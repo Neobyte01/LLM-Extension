4433aee7a281b7bddaa09139ea6791b9b1065d91
Sami Noroozi <68302126+potatohunter69@users.noreply.github.com>
2023-12-22 18:24:09 +0100

    now you can ask the chat bot by simply using the get_response function

--- api.py
+++ api.py
@@ -1,12 +1,51 @@
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
 
+
+
+# API key and client configuration
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
 client = OpenAI(
-    api_key="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"
+    api_key= api_key
 )
 
-response = client.chat.completions.create(
+
+
+# Function to get a response from the OpenAI chatbot
+def get_response(massage, rule): 
+
+    # Create a chat completion request using the OpenAI API
+    response = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "how are you?"}],
-)
+    messages=[
+        {
+        "role": "system",
+        "content": rule
+        },
+        {
+        "role": "user",
+        "content": massage
+        }
+    ],
+    stream=True,
+    temperature=0.7,
+    max_tokens=64,
+    top_p=1
+    )
 
+    # return the response
+    output = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            output += chunk.choices[0].delta.content
+    return output
+
+
+
+# example
+response = get_response("how are are and what are you doint?", "rewrite this qusetion")
 print(response)
+
